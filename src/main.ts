9ba4fe03eb5aa271e8a1ad8bc26da5541c82c327
Spelchure <alperenbayramoglu2@gmail.com>
2023-07-30 10:21:49 +0300

    feat: initial schema-less not working template

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,28 @@
-console.log("Hello world\n");
+import type { AppContext } from "./context";
+import express from "express";
+import { ApolloServer } from "@apollo/server";
+import cors from "cors";
+import { json } from "body-parser";
+import { expressMiddleware } from "@apollo/server/express4";
+
+const startServer = async () => {
+  // @ts-ignore
+  const server = new ApolloServer<AppContext>({ schema: undefined });
+
+  const app = express();
+
+  await server.start();
+
+  app.use(
+    "/graphql",
+    cors<cors.CorsRequest>(),
+    json(),
+    expressMiddleware(server)
+  );
+
+  app.listen(4000);
+
+  console.log(`🚀 Server listening on port 4000`);
+};
+
+startServer();
